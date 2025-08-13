efdd858b1493f7a498a10f818b6e70af6a34a27e
Rahulparihar01 <Rahulparihar.stevesai@gmail.com>
2025-08-13 21:56:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,20 +19,6 @@ credentials = service_account.Credentials.from_service_account_info(credentials_
 client = bigquery.Client(credentials=credentials, project=credentials.project_id)
 dataset_id = "CyberSecurity"
 
-# @app.get("/get-data")
-# def get_data():
-#     table="FinalNews3"
-#     full_table_id = f"{client.project}.{dataset_id}.{table}"
-
-#     try:
-#         query = f"SELECT * FROM `{full_table_id}`"
-#         df = client.query(query).to_dataframe()
-
-#         return JSONResponse(content=df.to_dict(orient="records"))
-
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-
 @app.get("/get-finalnew3-data")
 def get_data(
     # table: str = Query(..., description="Table name inside the CyberSecurity dataset"),
@@ -128,39 +114,6 @@ def get_data(
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": str(e)})
 
-
-
-# @app.get("/get-CWEs-data")
-# def get_data(
-#     # table: str = Query(..., description="Table name inside the CyberSecurity dataset"),
-#     page: int = Query(1, ge=1),
-#     page_size: int = Query(10, ge=1)
-# ):
-#     table= "CWEs"
-#     full_table_id = f"{client.project}.{dataset_id}.{table}"
-
-#     try:
-#         offset = (page - 1) * page_size
-#         query = f"SELECT * FROM `{full_table_id}` LIMIT {page_size + 1} OFFSET {offset}"
-#         df = client.query(query).to_dataframe()
-
-#         # Check if there are more rows
-#         has_more = len(df) > page_size
-#         df = df.head(page_size)  # Trim to page_size if we fetched extra
-
-#         return JSONResponse(content={
-#             "page": page,
-#             "page_size": page_size,
-#             "next_page": page + 1 if has_more else None,
-#             "has_more": has_more,
-#             "records": df.to_dict(orient="records")
-#         })
-
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-
-
-
 @app.get("/get-cves-data")
 def get_data(
     # table: str = Query(..., description="Table name inside the CyberSecurity dataset"),
@@ -221,6 +174,37 @@ def get_data(
             "next_page": page + 1 if has_more else None,
             "has_more": has_more,
             "records": records
+        })
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+
+@app.get("/get-cpes-data")
+def get_data(
+    # table: str = Query(..., description="Table name inside the CyberSecurity dataset"),
+    page: int = Query(1, ge=1),
+    page_size: int = Query(10, ge=1)
+):
+    table= "cpes"
+    full_table_id = f"{client.project}.{dataset_id}.{table}"
+
+    try:
+        offset = (page - 1) * page_size
+        query = f"SELECT * FROM `{full_table_id}` LIMIT {page_size + 1} OFFSET {offset}"
+        df = client.query(query).to_dataframe()
+
+        # Check if there are more rows
+        has_more = len(df) > page_size
+        df = df.head(page_size)  # Trim to page_size if we fetched extra
+
+        return JSONResponse(content={
+            "page": page,
+            "page_size": page_size,
+            "next_page": page + 1 if has_more else None,
+            "has_more": has_more,
+            "records": df.to_dict(orient="records")
         })
 
     except Exception as e:
